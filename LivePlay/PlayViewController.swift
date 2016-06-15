5d4fb6985a7fe6dcaa7a33d08ac580e5cc69c00d
swiftc-org <a@swiftc.org>
2016-06-15 20:04:58 +0800

    build ui

--- LivePlay/PlayViewController.swift
+++ LivePlay/PlayViewController.swift
@@ -15,11 +15,63 @@ class PlayViewController: UIViewController {
 
         view.backgroundColor = UIColor.whiteColor()
         
+        view.addSubview(connectButton)
+        
+        view.addSubview(playButton)
+    }
+    
+    private let commonBtnW = CGFloat(100)
+    private let commonBtnH = CGFloat(40)
+    
+    private var connectButton: UIButton {
+        
+        let buttonY = view.bounds.height - commonBtnH
+        let buttonF = CGRect(x: 0,
+                             y: buttonY,
+                             width: commonBtnW,
+                             height: commonBtnH)
+        
+        let button = UIButton(frame: buttonF)
+        button.backgroundColor = UIColor.greenColor()
+        button.setTitle("encode", forState: .Normal)
+        button.addTarget(self,
+                         action: #selector(connect),
+                         forControlEvents: .TouchUpInside)
+        
+        return button
+    }
+    
+    private var playButton: UIButton {
+        
+        let buttonX = view.bounds.width - commonBtnW
+        let buttonY = view.bounds.height - commonBtnH
+        let buttonF = CGRect(x: buttonX,
+                             y: buttonY,
+                             width: commonBtnW,
+                             height: commonBtnH)
+        
+        let button = UIButton(frame: buttonF)
+        button.backgroundColor = UIColor.greenColor()
+        button.setTitle("play", forState: .Normal)
+        button.addTarget(self,
+                         action: #selector(play),
+                         forControlEvents: .TouchUpInside)
+        
+        return button
+    }
+    
+    dynamic func connect() {
+        print(#function)
+    }
+    
+    dynamic func play() {
+        print(#function)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        
     }
     
 }
